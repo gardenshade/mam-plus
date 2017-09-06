5f4d4adf601be29d8a33e02e62d2b5c0b3e3a3c5
gardenshade <some.shaded.garden@gmail.com>
2017-09-06 14:00:42 -0600

    Bumped version number to 3.0.4

--- src/block_first.js
+++ src/block_first.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MAM Plus
 // @namespace    https://github.com/gardenshade
-// @version      3.0.3
+// @version      3.0.4
 // @description  Lots of tiny fixes for MAM
 // @author       GardenShade
 // @include      https://myanonamouse.net/*
