3d7e8291c408d13d25dcd685ed6fe5690b2352d7
gardenshade <some.shaded.garden@gmail.com>
2021-09-28 19:20:44 -0600

    fixed JSON requests failing

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,7 @@ namespace MP {
         UPDATE_LIST: [
             `🆕: Updated Ratio Protect to v1.8; this version adds a Cost To Restore Ratio info field.`,
             `🆕: Added option to pin the navigation/search area to the top of the page. Thanks @boomboxnation!`,
+            `🐞: Fixed an issue where all features that got data from MAM failed to work.`,
         ] as string[],
         BUG_LIST: [] as string[],
     };
